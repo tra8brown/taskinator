20bbb717238e39ba943a56da470f7bb32995a60a
TraBee <tra8brown@gmail.com>
2021-05-17 19:16:37 -0600

    add ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//hi:)
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
@@ -5,6 +6,18 @@ var taskFormHandler = function(event) {
     event.preventDefault(); //4.2.5 tells browser to not to refresh/default browser behavior (and keep remainder in text box)
     var taskNameInput = document.querySelector("input[name='task-name']").value; //4.2.6
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //check if input values are empty strings 4.2.8
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset(); //4.2.8
+
+    // reset form fields for next task to be entered (from snapcode)
+    document.querySelector("input[name='task-name']").value = "";
+    document.querySelector("select[name='task-type']").selectedIndex = 0;
 
     //package up data as an object 4.2.7
     var taskDataObj = {
@@ -14,7 +27,7 @@ var taskFormHandler = function(event) {
 
     //send it as an arguement to createTaskEl
     createTaskEl(taskDataObj);
-}
+};
 
 //taskDataObj
 var createTaskEl = function(taskDataObj) {
@@ -26,12 +39,21 @@ var createTaskEl = function(taskDataObj) {
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-
     listItemEl.appendChild(taskInfoEl);
 
-    // add entire list item to list
-    tasksToDoEl.appendChild(listItemEl);
+    console.dir(listItemEl); //(from snapcode)
 
-}
+    // add entire list item to list
+
+    // JB New code
+    // Vanilla JS
+    var ul = document.getElementById("tasks-to-do");
+    ul.appendChild(listItemEl);
+
+    // tracees old code
+    // jquery
+    // tasksToDoEl.appendChild(listItemEl);
+
+};
 
 formEl.addEventListener("submit", taskFormHandler); //on a button click, create a task
